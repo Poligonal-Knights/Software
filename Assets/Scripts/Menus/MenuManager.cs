bc392ec68b9f55de8df21e2d07bc814acf42dfdd
JorgeCambero <j.cambero.2019@alumnos.urjc.es>
2022-10-03 18:29:50 +0200

    Menú principal
    Se ha añadido una escena y un script para manejar el menú principal y la salida de la aplicación

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.LogWarning("Me salgo");
+    }
+}
