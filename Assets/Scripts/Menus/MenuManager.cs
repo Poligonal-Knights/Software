8d75d7a9c72f02dfe0869601bbc2778c7e368471
JorgeCambero <j.cambero.2019@alumnos.urjc.es>
2022-12-03 23:47:59 +0100

    Added mode select and lvl select menus

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -21,14 +21,25 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
-    public void OpenOptionP()
+    public void StoryMode()
     {
-        //SceneManager.LoadScene("OpcionesP");
+        SceneManager.LoadScene(2); 
+    }
+    public void Lvl1()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Lvl2()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Lvl3()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Lvl4()
+    {
+        SceneManager.LoadScene(6);
     }
 
-    public void ExitGame()
-    {
-        Application.Quit();
-        Debug.LogWarning("Me salgo");
-    }
 }
