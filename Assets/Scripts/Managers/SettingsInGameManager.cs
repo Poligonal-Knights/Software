77b648e74127bc169acef1885b15f44f6098038a
Whikkot <93128672+Whikkot@users.noreply.github.com>
2022-12-07 21:09:59 +0100

    En proceso de hacer funcionar los botones
    Para que Juan pueda trabajar mejor al haber cambios en la jerarquía base de los canvas.,

--- Assets/Scripts/Managers/SettingsInGameManager.cs
+++ Assets/Scripts/Managers/SettingsInGameManager.cs
@@ -41,7 +41,6 @@ public class SettingsInGameManager : MonoBehaviour
     public Canvas petraAb4Canvas;
     public Canvas petraPasiveCanvas;
     //FINPETRA
-    public Canvas enemiesCanvas;
     //ENEMIES
     public Canvas enemyCanvas;
     public Canvas trashmobCanvas;
@@ -61,7 +60,7 @@ public class SettingsInGameManager : MonoBehaviour
         allControlCanvas= new List<Canvas>();
         allControlCanvas.Add(initialCanvas);
         allControlCanvas.Add(controlCanvas);
-        //Añadir canvas de Magnus
+        //Aï¿½adir canvas de Magnus
         allControlCanvas.Add(magnusCanvas);
         allControlCanvas.Add(magnusBasicCanvas);
         allControlCanvas.Add(magnusPasiveCanvas);
@@ -69,8 +68,8 @@ public class SettingsInGameManager : MonoBehaviour
         allControlCanvas.Add(magnusAb2Canvas);
         allControlCanvas.Add(magnusAb3Canvas);
         allControlCanvas.Add(magnusAb4Canvas);
-        //FIN DE Añadir canvas de Magnus
-        //Añadir canvas de Adel
+        //FIN DE Aï¿½adir canvas de Magnus
+        //Aï¿½adir canvas de Adel
         allControlCanvas.Add(adelCanvas);
         allControlCanvas.Add(adelBasicCanvas);
         allControlCanvas.Add(adelPasiveCanvas);
@@ -78,8 +77,8 @@ public class SettingsInGameManager : MonoBehaviour
         allControlCanvas.Add(adelAb2Canvas);
         allControlCanvas.Add(adelAb3Canvas);
         allControlCanvas.Add(adelAb4Canvas);
-        //FIN DE Añadir canvas de Adel
-        //Añadir canvas de Petra
+        //FIN DE Aï¿½adir canvas de Adel
+        //Aï¿½adir canvas de Petra
         allControlCanvas.Add(petraCanvas);
         allControlCanvas.Add(petraBasicCanvas);
         allControlCanvas.Add(petraPasiveCanvas);
@@ -87,8 +86,8 @@ public class SettingsInGameManager : MonoBehaviour
         allControlCanvas.Add(petraAb2Canvas);
         allControlCanvas.Add(petraAb3Canvas);
         allControlCanvas.Add(petraAb4Canvas);
-        //FIN DE Añadir canvas de Petra
-        //Añadir canvas de Kelsier
+        //FIN DE Aï¿½adir canvas de Petra
+        //Aï¿½adir canvas de Kelsier
         allControlCanvas.Add(kelsierCanvas);
         allControlCanvas.Add(kelsierBasicCanvas);
         allControlCanvas.Add(kelsierPasiveCanvas);
@@ -96,21 +95,42 @@ public class SettingsInGameManager : MonoBehaviour
         allControlCanvas.Add(kelsierAb2Canvas);
         allControlCanvas.Add(kelsierAb3Canvas);
         allControlCanvas.Add(kelsierAb4Canvas);
-        //FIN DE Añadir canvas de Kelsier
-        //Añadir canvas de enemigos
+        //FIN DE Aï¿½adir canvas de Kelsier
+        //Aï¿½adir canvas de enemigos
         allControlCanvas.Add(trashmobCanvas);
         allControlCanvas.Add(mageCanvas);
         allControlCanvas.Add(supportCanvas);
         allControlCanvas.Add(archerCanvas);
         allControlCanvas.Add(tankCanvas);
         allControlCanvas.Add(kamikazeCanvas);
-        allControlCanvas.Add(enemiesCanvas);
-        //FIN DE añadir canvas de enemigos
+        allControlCanvas.Add(enemyCanvas);
+        //FIN DE aï¿½adir canvas de enemigos
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ShowThisCanvas(Canvas canvasToShow)
+    {
+        foreach (Canvas c in allControlCanvas)
+        {
+            c.gameObject.SetActive(false);
+        }
+        //previousCanvas = currentCanvas;
+        //currentCanvas = canvasToShow;
+        canvasToShow.gameObject.SetActive(true);
+        //if(GameManager.turnManager.IsPlayerTurn()) turnCanvas.gameObject.SetActive(true);
+        // alwaysActiveCanvas.gameObject.SetActive(true);
+    }
+
+    public void HideAllSettings()
+    {
+        foreach (Canvas c in allControlCanvas)
+        {
+            c.gameObject.SetActive(false);
+        }
+        this.gameObject.SetActive(false);
     }
 }
