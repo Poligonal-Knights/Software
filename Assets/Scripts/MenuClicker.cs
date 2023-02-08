b5ea6d7e54cac671ac5c96bc13bee972a0f142c0
JorgeCambero <j.cambero.2019@alumnos.urjc.es>
2023-02-08 16:32:34 +0100

    Added extra info to de lvl select

--- Assets/Scripts/MenuClicker.cs
+++ Assets/Scripts/MenuClicker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class MenuClicker : MonoBehaviour
@@ -19,6 +20,8 @@ public class MenuClicker : MonoBehaviour
     public GameObject lvl6;
     public GameObject lvl7;
     public GameObject lvl8;
+    public Sprite a,b,c,d;
+
     Vector3 oldPos;
     // Start is called before the first frame update
     void Start()
@@ -42,35 +45,74 @@ public class MenuClicker : MonoBehaviour
 
                     if (GameObject.ReferenceEquals(hit.transform.gameObject,lvl1))
                     {
-                        
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl1());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 1");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Pequeño");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = a;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("2");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("7");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl1());
                     }else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl2))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl2());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 2");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Pequeño");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = a;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("2");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("5");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl2());
                     }
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl3))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl3());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 3");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Pequeño");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = b;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("11");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl3());
                     }
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl4))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl4());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Mediano");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = b;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("11");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl4());
                     }//
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl5))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl5());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 5");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Mediano");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = c;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("12");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl5());
                     }
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl6))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl6());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 6");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Grande");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = c;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("13");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl6());
                     }
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl7))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl7());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 7");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Grande");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = d;
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("13");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl7());
                     }
                     else if (GameObject.ReferenceEquals(hit.transform.gameObject, lvl8))
                     {
-                        selectedCanvas.transform.Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl8());
+                        selectedCanvas.transform.Find("Fondo").GetChild(0).GetComponent<TextMeshProUGUI>().SetText("Mundo 1 Nivel 8");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetComponent<TextMeshProUGUI>().SetText("Grande");
+                        selectedCanvas.transform.Find("Fondo").GetChild(1).GetChild(0).GetComponent<UnityEngine.UI.Image>().sprite = d; 
+                        selectedCanvas.transform.Find("Fondo").GetChild(2).GetComponent<TextMeshProUGUI>().SetText("4");
+                        selectedCanvas.transform.Find("Fondo").GetChild(3).GetComponent<TextMeshProUGUI>().SetText("13");
+                        selectedCanvas.transform.Find("Fondo").Find("SiBt").GetComponent<Button>().onClick.AddListener(() => MenuManager.Instance.Lvl8());
                     }
 
 
